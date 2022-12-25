6fe61cb6135ab9309c975222965c8c8f6f504a13
Asqanaz <asmoyan.04@gmail.com>
2022-12-25 13:54:17 +0400

    update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,33 +1,28 @@
 import React, { useState } from "react"
 import { todoAPI } from "../services/TodosService"
 
-export const TodoItem = ({ title, item, value }) => {
+export const TodoItem = ({ title, item }) => {
 	const [updateTodo, {}] = todoAPI.useUpdateTodoMutation()
 	const [deleteTodo, {}] = todoAPI.useDeleteTodoMutation()
+	const [singleValue, setSingleValue] = useState(title)
 	const [checkboxSwitch, {}] = todoAPI.useCheckboxSwitchMutation()
 
-	const handleEditTask = e => {
-		updateTodo({ ...item, title: e.target.value || title })
-	}
-
-	const handleDelete = () => {
-		deleteTodo(item)
-	}
-
-	const handleCheckbox = () => {
-		checkboxSwitch({ ...item, completed: !item.completed })
-	}
-
+	console.log(singleValue)
 	return (
 		<div className="flex flex-row items-center gap-6">
 			<input
 				type="text"
-				defaultValue={title}
+				value={singleValue}
 				className={`outline-none border-none ${item.completed ? "text-green-600" : "text-black"}`}
-				onBlur={handleEditTask}
+				onBlur={() => updateTodo({ ...item, title: singleValue.trim() || title })}
+				onChange={e => setSingleValue(e.target.value)}
 			/>
-			<input type="checkbox" checked={item.completed} onChange={handleCheckbox} />
-			<button onClick={handleDelete}>Delete</button>
+			<input
+				type="checkbox"
+				checked={item.completed}
+				onChange={() => checkboxSwitch({ ...item, completed: !item.completed })}
+			/>
+			<button onClick={() => deleteTodo(item)}>Delete</button>
 		</div>
 	)
 }
